5bb36447adf11eb74677305c2cd7166681d377b7
pdt0510 <phanductai920510@gmail.com>
2023-07-11 17:42:45 +0700

    ...TS 3 Decorators course; AOP; 4 decorators types; Multiple decorators; debugging decorators; Express Routes with Decorators; Decorator factory; Decorator Metadata; - from src1 -> 2

--- src1/main1.ts
+++
@@ -1,8 +0,0 @@
-//main1
-let junkDrawer: any[] = ['cool string', 42, true]; //1m14ss
-
-let companies: Array<string> = ['Microsoft', 'Google', 'Amazon']; //1m37ss
-
-let primeNums: Array<number> = [7, 11, 13]; //3ms13ss
-
-let lastValue = primeNums.pop();
